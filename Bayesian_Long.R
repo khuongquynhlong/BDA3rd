c1e3a850c12b569a6c96726c38ee149f4ac54072
Khuong Quynh Long <longkhuong.yds@gmail.com>
2022-12-16 15:40:38 +0100

    add multivariate model

--- Bayesian_Long.R
+++ Bayesian_Long.R
@@ -5,7 +5,7 @@ library(tidyverse)
 
 #---------- Read data
 
-df <- readRDS("GSPS.RData")
+df <- readRDS("Data/GSPS.RData")
 
 
 glimpse(df)
@@ -66,77 +66,95 @@ par(mfrow = c(1, 1))
 table(df$year)
 hist(df$educ)
 
+table(df$public)
+
+#---------- Standardized variables
+#===============================================================================
+std <- function(x){(x - mean(x))/sd(x)}
 
 #---------- Question 1
 #===============================================================================
 # Set up the data given
-
-model_data <- list(working = df$working,
-                   female = df$female,
-                   year = df$year,
-                   age = df$age,
-                   hsat = df$hsat,
-                   handdum = df$handdum,
-                   handper = df$handper,
-                   hhninc = df$hhninc,
-                   hhkids = df$hhkids,
-                   educ = df$educ,
-                   married = df$married,
-                   docvis = df$docvis,
-                   hospvis = df$hospvis,
-                   public = df$public)
+model_data <- list(working     = df$working,
+                   female      = df$female,
+                   year        = std(df$year),
+                   age         = std(df$age),
+                   hsat        = std(df$hsat),
+                   handdum     = df$handdum,
+                   handper     = std(df$handper),
+                   hhninc      = std(df$hhninc),
+                   hhkids      = df$hhkids,
+                   educ        = std(df$educ),
+                   married     = df$married,
+                   docvis      = std(df$docvis),
+                   hospvis     = std(df$hospvis),
+                   public      = df$public)
 model_constant <- list(N = nrow(df))
 
-
 # Initial value
-initial_values <- list(beta = c(0, 0))
+initial_values <- list(beta = rep(0, 13))
 
 # specify MCMC details
 params <- c("beta")
 
-
 # Vague prior
 #-------------------------------------------------------------------------------
 # Write nimble model, logistic regression
-logit_mod1 <- nimbleCode(
+logit_mod1_code <- nimbleCode(
   {
     # Data model
     for (i in 1:N) {
-      logit(p[i]) <- beta[1] + beta[2]*female[i]
+      logit(p[i]) <- beta[1] + beta[2]*female[i] + beta[3]*year[i] + beta[4]*age[i] + 
+        beta[5]*hsat[i] + beta[6]*handper[i] + beta[7]*hhninc[i] + beta[8]*hhkids[i] +
+        beta[9]*educ[i] + beta[10]*married[i] + beta[11]*docvis[i] + beta[12]*hospvis[i] +
+        beta[13]*public[i]
       working[i] ~ dbern(p[i])
     }
     # Prior
-    for (i in 1:2){
+    for (i in 1:13){
       beta[i]~ dnorm(0.0,1.0E-4)
     }
 })
 
 
-logit_mod1 <- nimbleMCMC(code = logit_mod1,
+logit_mod1 <- nimbleMCMC(code = logit_mod1_code,
                         data = model_data,
                         constants = model_constant,
                         inits = initial_values,
                         monitors = params,
                         niter = 5000,
                         nburnin = 1000,
-                        nchains = 3,
+                        nchains = 2,
                         thin = 1,
                         samplesAsCodaMCMC = TRUE,
                         WAIC = TRUE)
-
-
 
 # Convert  into mcmc.list 
 
 model1_mcmc <- as.mcmc.list(logit_mod1$samples)
 
+gelman.diag(model1_mcmc)
+gelman.plot(model1_mcmc)
+
 # Produce general summary of obtained MCMC sampling
 
 plot(model1_mcmc)
 summary(model1_mcmc)
+densplot(model1_mcmc)
+
+# Extract MCMC
+model1_mcmc %>% gather_draws(`beta[1]`) %>% median_hdi() %>% 
+  select(".value",".lower",".upper")
+
 
 # WAIC
 logit_mod1$WAIC
+
+
+
+
+
+
 
 
 
